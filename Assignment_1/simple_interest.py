2fe6048f61a0c5ee7b9ee134fe3ff96f84b20d28
Shivang Saxena <76741975+shivang-saxena-10@users.noreply.github.com>
2021-01-10 14:08:29 +0530

    Add files via upload

---
+++ Assignment_1/simple_interest.py
@@ -0,0 +1,5 @@
+p=float(input("Enter the principle amount : "))
+r=int(input("Enter the rate of interest : "))
+t=int(input("Enter the time period : "))
+si=(p*r*t)/100
+print("The simple interest is ",si)
